a176dcbfb5dc34097a81e56b276712863b4757a7
Ryan Henrique de Assis Belfort <70604408+RyanHenriqueBelfort@users.noreply.github.com>
2022-06-28 19:04:55 -0300

    subscriber responsive

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,8 +3,8 @@ export function Logo() {
   return (
     <>
       <div className="flex items-center">
-        <span className="text-5xl border-r-4 pr-4 text-green-500 font-bold">Event</span>
-        <span className="font-bold pl-2 text-6xl text-purple-600">ON</span>
+        <span className="text-3xl md:text-5xl border-r-4 pr-4 text-green-500 font-bold">Event</span>
+        <span className="font-bold pl-2 text-4xl md:text-6xl text-purple-600">ON</span>
       </div>
     </>
     // <svg
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -24,40 +24,42 @@ export function Subscribe() {
   }
 
   return(
-    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex justify-between items-center mt-20 mx-auto">
+    <div className="min-h-screen bg-blur bg-cover flex flex-col items-center">
+      <div className="pl-1 w-full max-w-[1100px] flex flex-col md:flex-row justify-between items-center mt-20 mx-auto">
         <div className="max-w-[640px]">
-          <Logo />
+          <div className="flex justify-center md:justify-start">
+            <Logo />
+          </div>
 
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
-            Guia de como <strong className="text-blue-500 text-5xl">React funciona</strong>, partindo do total do <strong className="text-blue-500 text-5xl" >zero</strong>
+          <h1 className="mt-8 text-lg md:text-[2.5rem] leading-tight text-center">
+            Guia de como <strong className="text-blue-500 text-2xl md:text-5xl">React funciona</strong>, partindo do total do <strong className="text-blue-500 text-2xl md:text-5xl" >zero</strong>
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="mt-4 mb-4 text-gray-200 leading-relaxed text-xs md:text-xl text-center">
             Alguns matérias para voce entender de vez por todas de como uma das 
             tecnologias mais demandada do mercado e todo seu universo funciona.
             Através de conteúdos gratuitos pela internet.
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-          <strong className="text-2xl mb-6 block">Inscreva gratuitamente</strong>
+        <div className="p-2 md:p-8 bg-gray-700 border border-gray-500 rounded w-50 md:w-1/3">
+          <strong className="text-xs md:text-2xl mb-6 block">Inscreva gratuitamente</strong>
 
           <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
             <input 
-              className="bg-gray-900 rounded px-5 h-14"
+              className="bg-gray-900 rounded px-5 h-8 md:h-14 placeholder:text-xs md:placeholder:text-base" 
               type="text" 
               placeholder="Seu nome completo"
               onChange={event => setName(event.target.value)} 
             />
             <input
-              className="bg-gray-900 rounded px-5 h-14"
+              className="bg-gray-900 rounded px-5 h-8 md:h-14 placeholder:text-xs md:placeholder:text-base"
               type="email"
               placeholder="Digite seu melhor email" 
               onChange={event => setEmail(event.target.value)} 
             />
 
             <button 
-              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-20"
+              className="mt-4 bg-green-500 uppercase py-2 rounded font-bold text-xs md:text-sm hover:bg-green-700 transition-colors disabled:opacity-20 h-8 md:h-14"
               type="submit"
               disabled={loading}
             >
